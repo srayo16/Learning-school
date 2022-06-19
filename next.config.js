8dfdfe8d6ca42519153e3037e8c6859d9d5c0e1b
razuahmedjoy <rjjoybzs420@gmail.com>
2022-06-19 17:22:36 +0600

    added achievement section

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains:['cdn.10minuteschool.com','10minuteschool.com'],
+  }
 }
 
 module.exports = nextConfig

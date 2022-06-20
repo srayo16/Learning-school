286b50a2e99f99d50034f882dfccdcda95599088
MainUddin1223 <2021md.main@gmail.com>
2022-06-20 07:00:51 +0600

    routing completed

---
+++ pages/courses/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Courses from '../../components/Courses/Courses'
+
+export default function index() {
+  return (
+    <div>
+        <Courses/>
+    </div>
+  )
+}
